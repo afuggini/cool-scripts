295b40396146e16e98b516926cbb6d7552693b13
Ariel Fuggini <afuggini@users.noreply.github.com>
2023-04-14 13:46:43 -0300

    adds motivational music

--- tinder-auto-swipe/tinder-auto-swipe.js
+++ tinder-auto-swipe/tinder-auto-swipe.js
@@ -1,5 +1,7 @@
 (function autoSwipe() {
   /* START CUSTOMIZING HERE */
+  const PLAY_MOTIVATION_MUSIC_IN_BG = false;
+  const MUSIC_YOUTUBE_VIDEO_ID = 'PSoJvb3qEqs';
   const RIGHT_SWIPE_RATIO = 0.8;
   const LIKE_SELECTOR = '.Bdc\\(\\$c-ds-border-gamepad-like-default\\) button';
   const NOPE_SELECTOR = '.Bdc\\(\\$c-ds-border-gamepad-nope-default\\) button';
@@ -9,6 +11,16 @@
   const PAUSE_MIN_BETWEEN_SWIPES = 1000;
   const PAUSE_MAX_BETWEEN_SWIPES = 2000;
   /* END CUSTOMIZING HERE */
+
+  const playInBackground = (videoId) => {
+    const iframe = document.createElement('iframe');
+    iframe.width = 0;
+    iframe.height = 0;
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}`;
+    iframe.allow = 'autoplay';
+    iframe.style.visibility = 'hidden';
+    document.body.appendChild(iframe);
+  }  
 
   const getRandomTimeout = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
   const performSwipe = () => {
@@ -27,4 +39,6 @@
   };
 
   swipe();
+
+  PLAY_MOTIVATION_MUSIC_IN_BG && playInBackground(MUSIC_YOUTUBE_VIDEO_ID);
 })();
